20798126f400ab5e5cd3d907e276bcfa53ff9bd2
Sam Toms <sam.toms@esneft.nhs.uk>
2024-06-07 15:50:58 +0000

    Add config and github issues module

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -8,7 +8,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args()
+    |> process()
   end
 
   @doc """
@@ -20,13 +22,41 @@ defmodule Issues.CLI do
   Return a tuple of `{user, project, count}`, or `:help` if help was given.
   """
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+    OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+    |> IO.inspect()
+    |> elem(1)
+    |> args_to_internal_representation()
+  end
 
-    case parse do
-      {[help: true], _, _} -> :help
-      {_, [user, project, count], _} -> {user, project, String.to_integer(count)}
-      {_, [user, project], _} -> {user, project, @default_count}
-      _ -> :help
-    end
+  def args_to_internal_representation([user, project, count]) do
+    {user, project, String.to_integer(count)}
+  end
+
+  def args_to_internal_representation([user, project]) do
+    {user, project, @default_count}
+  end
+
+  def args_to_internal_representation(_) do
+    :help
+  end
+
+  def process(:help) do
+    IO.puts("""
+    usage: issues <user> <project> [count | #{@default_count}]
+    """)
+
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts("Error fetching from Github: #{error}")
+    System.halt(2)
   end
 end
---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,28 @@
+defmodule Issues.GithubIssues do
+  @user_agent [{"User-agent", "Elixir muel.toms@gmail.com"}]
+  @github_url Application.compile_env(:issues, :github_url)
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response()
+  end
+
+  def issues_url(user, project) do
+    "#{@github_url}/#{user}/#{project}/issues"
+  end
+
+  def handle_response({_, %{status_code: status_code, body: body}}) do
+    {
+      status_code |> check_for_error(),
+      body |> Jason.decode()
+    }
+  end
+
+  defp check_for_error(200), do: :ok
+  defp check_for_error(_), do: :error
+
+  # def handle_response({_, %{status_code: _, body: body}}) do
+  #   {:error, body}
+  # end
+end
